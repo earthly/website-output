ca7e3eb7a10bc2d89200ac0526c9f33df74c585b
Automated <actions@users.noreply.github.com>
2021-06-01 18:57:28 +0000

    Latest data:

--- blog/assets/js/lunr/lunr-store.js
+++ blog/assets/js/lunr/lunr-store.js
@@ -286,4 +286,28 @@ var store = [{
         "tags": [],
         "url": "/blog/achieving-repeatability/",
         "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Creating and hosting your own deb packages and apt repo",
+        "excerpt":"As an Ubuntu user, I find myself typing apt install ... frequently as a way to install software on my system. But what if I wanted to distribute my code to others via an apt repository? In this post I’ll cover how to 1) create a deb package, 2) create...","categories": ["Tutorials"],
+        "tags": [],
+        "url": "/blog/creating-and-hosting-your-own-deb-packages-and-apt-repo/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Using Autotools to Configure, Make, and Install a Program",
+        "excerpt":"Autotools is one of the most widely adopted code packaging and shipping tools available to developers on Linux. While there are alternatives, such as CMake, SCons, and BJam, they don’t quite match Autotools in ease of use, power, and versability. At its base, Autotools can help make your application more...","categories": ["Tutorial"],
+        "tags": [],
+        "url": "/blog/autoconf/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Example Graphs",
+        "excerpt":"You can embedded graphs into the markdown like this: Simple Graph More Complex How to install pandoc-plot If you are using the docker container and jekyll, it should just work. Manual Mac setup: brew install pandoc brew install pandoc-plot install specific libs: pandoc-plot toolkits to list pip3 install matplotlib for...","categories": ["Tutorials"],
+        "tags": [],
+        "url": "/blog/example-plot/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Example Post",
+        "excerpt":"This post is in the future, and won’t show up in the published site Image without figure An image with the alt text hidden. An image with alt text printed below. some alt text Image with explicit figure Alex likes to draw and add a caption Image with implicit figure...","categories": ["News"],
+        "tags": [],
+        "url": "/blog/example/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
       }]
