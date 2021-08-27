eb7e6184b024bef910419e6ed014ee8012b14cb9
Automated <actions@users.noreply.github.com>
2021-08-27 16:05:06 +0000

    Latest data:

--- blog/assets/js/lunr/lunr-store.js
+++ blog/assets/js/lunr/lunr-store.js
@@ -448,4 +448,28 @@ var store = [{
         "tags": [],
         "url": "/blog/jq-select/",
         "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Understanding and Using Makefile Flags",
+        "excerpt":"make is a commonplace utility in the development world. It automates the process of generating executables, documentations, and other non-source files from the source code by dividing the build process into separate interrelated steps. Using make eliminates the need for typing out long and complex commands to compile the source...","categories": ["Tutorials"],
+        "tags": [],
+        "url": "/blog/make-flags/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Makefile Tutorials and Examples to Build From",
+        "excerpt":"Building software is a multi-step process—installing or updating dependencies, compiling the source code, testing, installing, and so on. In any moderately sized project, you might find it difficult to perform all these steps manually. This is where make can help you. The make tool automates compilation of the software from...","categories": ["Tutorials"],
+        "tags": [],
+        "url": "/blog/make-tutorial/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "10+ Best Continuous Delivery Tools",
+        "excerpt":"Draft.dev Article Checklist Add in Author page Create header image in Canva Optional: Find ways to break up content with quotes or images Verify look of article locally Run mark down linter (earthly +blog-lint-apply) Add keywords for internal links to front-matter Run earthly +link-opportunity and find 1-5 places to incorporate...","categories": ["Tutorials"],
+        "tags": [],
+        "url": "/blog/10-continous-delivery/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
+      },{
+        "title": "Example Post",
+        "excerpt":"This post is in the future, and won’t show up in the published site Image without figure An image with the alt text hidden. An image with alt text printed below. some alt text Image with explicit figure Alex likes to draw and add a caption Image with implicit figure...","categories": ["News"],
+        "tags": [],
+        "url": "/blog/example/",
+        "teaser": "/blog/assets/images/default-teaser.jpg"
       }]
